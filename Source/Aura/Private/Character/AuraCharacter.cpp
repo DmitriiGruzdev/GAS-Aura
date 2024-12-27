cfa4c996648d148120878c6006181b5b49f3adf7
DmitriiGruzdev <d1gruzdev@gmail.com>
2024-12-27 17:24:08 +0100

    Finished Section 8

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -38,6 +38,13 @@ AAuraCharacter::AAuraCharacter()
 	bUseControllerRotationYaw = false;
 }
 
+int32 AAuraCharacter::GetPlayerLevel()
+{
+	const AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	return AuraPlayerState->GetPlayerLevel();
+}
+
 void AAuraCharacter::InitAbilityActorInfo()
 {
 	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
@@ -54,6 +61,7 @@ void AAuraCharacter::InitAbilityActorInfo()
 			AuraHUD->InitOverlay(AuraPlayerController,GetPlayerState(),AbilitySystemComponent,AttributeSet);	
 		}
 	}
+	InitializeDefaultAttributes();
 }
 
 
